14eec480dadd99c60afd919d5f86604a10c30fd0
AeswellClinic <clinicaeswell@gmail.com>
2024-09-17 20:21:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const card = cards.querySelector('.card');
     const gap = 20;
     const cardWidth = card.offsetWidth;
-
     function handleScrollNext() {
         cards.scrollBy({
             top: 0,
             left: cardWidth + gap,
             behavior: 'smooth'
         });
-
         setTimeout(() => {
             const firstCard = cards.querySelector('.card');
             cards.appendChild(firstCard);
             cards.scrollLeft -= cardWidth + gap;
         }, 300);
     }
-
     function handleScrollPrev() {
         const lastCard = cards.lastElementChild;
         cards.insertBefore(lastCard, cards.firstElementChild);
@@ -33,11 +30,9 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         }, 100);
     }
-
     function autoSlide() {
         handleScrollNext();
     }
-
     const autoSlideInterval = setInterval(autoSlide, 3000);
 
     next.addEventListener('click', function() {
